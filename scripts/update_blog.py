ca13f7850914453d09e8185bb28a8f38aaf04c3f
김민철(MInCheol Kim) <90129212+gener-lst@users.noreply.github.com>
2024-08-13 00:40:01 +0900

    Update update_blog.py

--- scripts/update_blog.py
+++ scripts/update_blog.py
@@ -4,7 +4,7 @@ import os
 
 # 벨로그 RSS 피드 URL
 # example : rss_url = 'https://api.velog.io/rss/@soozi'
-rss_url = 'https://api.velog.io/rss/@[gener-lst]'
+rss_url = 'https://api.velog.io/rss/@gener-lst'
 
 # 깃허브 레포지토리 경로
 repo_path = '.'
